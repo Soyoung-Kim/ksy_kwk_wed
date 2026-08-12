245f2c885bc4d4c53ca6e4b41fe322059c569f7f
soyoung <syoung@nongshim.co.kr>
2026-08-12 14:38:40 +0900

    config: add Kakao JavaScript key

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ export const APP_CONFIG = {
 
   share: {
     // Kakao Developers > App settings > Platform > Web에 GitHub Pages 주소를 등록한 뒤 입력하세요.
-    kakaoJavaScriptKey: '',
+    kakaoJavaScriptKey: '8b93eba907a57c775af83d0da7a5bbaa',
     title: '소영 ❤ 우경 결혼합니다',
     description: '2026.11.08 SUN AM 11:30',
     imagePath: './assets/photos/main_logo.png'
